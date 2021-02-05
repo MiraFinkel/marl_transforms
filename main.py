ea597fc20da7cc810f03e065c18bd3187a8889e6
MiraFinkel <mirafinkel@gmail.com>
2021-02-05 10:26:28 +0200

    changes in utils.py

--- main.py
+++ main.py
@@ -1,52 +1,7 @@
-import ray
 from utils import *
 from visualize import *
-import Environments.MultiTaxiEnv.multitaxienv.taxi_environment as taxi_env
-
-s = "{:3d} reward {:6.2f}/{:6.2f}/{:6.2f} len {:6.2f}"
 
 
-def train(env_name, agent_name, with_transform=False, transform_idx=0):
-    env, env_to_agent = get_env(env_name, with_transform, transform_idx)
-    ray.init(num_gpus=NUM_GPUS, local_mode=WITH_DEBUG)
-    config = get_taxi_config(env, NUM_TAXI)
-    agent = get_agent(agent_name, config, env_to_agent)
-    episode_reward_mean = []
-    for it in range(ITER_NUM):
-        result = agent.train()
-        print(s.format(
-            it + 1,
-            result["episode_reward_min"],
-            result["episode_reward_mean"],
-            result["episode_reward_max"],
-            result["episode_len_mean"]
-        ))
-        episode_reward_mean.append(result["episode_reward_mean"])
-    evaluate(env, agent, config)
-    ray.shutdown()
-    return episode_reward_mean
-
-
-def evaluate(env, agent, config):
-    print(" ===================================================== ")
-    print(" ================ STARTING EVALUATION ================ ")
-    print(" ===================================================== ")
-
-    taxi_env.set_display(True)
-    obs = env.reset()
-    done = False
-    episode_reward = 0
-    while not done:
-        action = {}
-        for agent_id, agent_obs in obs.items():
-            policy_id = config['multiagent']['policy_mapping_fn'](agent_id)
-            action[agent_id] = agent.compute_action(agent_obs, policy_id=policy_id)
-        obs, reward, done, info = env.step(action)
-        done = done['__all__']
-        # sum up reward for all agents
-        episode_reward += sum(reward.values())
-    print(episode_reward)
-    taxi_env.set_display(False)
 
 
 if __name__ == '__main__':
--- utils.py
+++ utils.py
@@ -2,6 +2,9 @@ from constants import *
 from Environments.MultiTaxiEnv.multitaxienv.taxi_environment import TaxiEnv
 import ray.rllib.agents.pg as pg
 import ray.rllib.agents.a3c as a3c
+import ray
+
+s = "{:3d} reward {:6.2f}/{:6.2f}/{:6.2f} len {:6.2f}"
 
 
 def get_env(env_name, with_transform=False, transform_idx=0):
@@ -34,3 +37,46 @@ def get_taxi_config(env, number_of_taxis):
                   "num_workers": NUM_WORKERS}
 
     return config
+
+
+def train(env_name, agent_name, with_transform=False, transform_idx=0):
+    env, env_to_agent = get_env(env_name, with_transform, transform_idx)
+    ray.init(num_gpus=NUM_GPUS, local_mode=WITH_DEBUG)
+    config = get_taxi_config(env, NUM_TAXI)
+    agent = get_agent(agent_name, config, env_to_agent)
+    episode_reward_mean = []
+    for it in range(ITER_NUM):
+        result = agent.train()
+        print(s.format(
+            it + 1,
+            result["episode_reward_min"],
+            result["episode_reward_mean"],
+            result["episode_reward_max"],
+            result["episode_len_mean"]
+        ))
+        episode_reward_mean.append(result["episode_reward_mean"])
+    evaluate(env, agent, config)
+    ray.shutdown()
+    return episode_reward_mean
+
+
+def evaluate(env, agent, config):
+    print(" ===================================================== ")
+    print(" ================ STARTING EVALUATION ================ ")
+    print(" ===================================================== ")
+
+    TaxiEnv.set_display(True)
+    obs = env.reset()
+    done = False
+    episode_reward = 0
+    while not done:
+        action = {}
+        for agent_id, agent_obs in obs.items():
+            policy_id = config['multiagent']['policy_mapping_fn'](agent_id)
+            action[agent_id] = agent.compute_action(agent_obs, policy_id=policy_id)
+        obs, reward, done, info = env.step(action)
+        done = done['__all__']
+        # sum up reward for all agents
+        episode_reward += sum(reward.values())
+    print(episode_reward)
+    TaxiEnv.set_display(False)
