7dc875aede8e4d0a9223e5d062c3aebc31cc579a
Mira Finkelstein <61774312+MiraFinkel@users.noreply.github.com>
2021-02-14 19:32:21 +0200

    Update utils.py

--- utils.py
+++ utils.py
@@ -1,48 +1,36 @@
 from constants import *
-from Environments.MultiTaxiEnv.multitaxienv.taxi_environment import TaxiEnv
-import Environments.MultiTaxiEnv.multitaxienv.taxi_environment as taxi_env
-from supersuit import pad_observations_v0, pad_action_space_v0
-from pettingzoo.mpe import simple_speaker_listener_v3
-from ray.tune.registry import register_env
-from ray.rllib.env import PettingZooEnv
 import numpy as np
 
-TAXI = "taxi"
-SPEAKER_LISTENER = "simple_speaker_listener"
 
-def get_env(env_name, number_of_agents=1, with_transform=False, transform=None, transform_idxes=None):
+def get_env(env_name, number_of_agents=1):
     """
     TODO Guy: to expand the function to work with particle environment
     :param env_name:
     :param number_of_agents:
-    :param with_transform:
-    :param transform:
-    :param transform_idxes:
     :return:
     """
-    global env
     if env_name == TAXI:
-        if not with_transform:
-            if env_name == TAXI:
-                taxi_env.set_number_of_agents(number_of_agents)
-                env = TaxiEnv()
-                return env, TaxiEnv
-        else:
-            taxi_env.set_number_of_agents(number_of_agents)
-            transform_env = TransformEnvironment()
-            env = TransformEnvironment()  # TODO Mira - do I need the transformed environment? or the original one?
-            transform_env._mapping_class.set_reduction_idx(transform_idxes)
-            return transform_env, TransformEnvironment
+        from Environments.MultiTaxiEnv.multitaxienv.taxi_environment import TaxiEnv
+        import Environments.MultiTaxiEnv.multitaxienv.taxi_environment as taxi_env
+        taxi_env.set_number_of_agents(number_of_agents)
+        env = TaxiEnv()
+        return env, TaxiEnv
     if env_name == SPEAKER_LISTENER:
         from supersuit import pad_observations_v0, pad_action_space_v0
+        from pettingzoo.mpe import simple_speaker_listener_v3
+        from ray.tune.registry import register_env
+        from ray.rllib.env import PettingZooEnv
+
         def create_env(args):
             env = simple_speaker_listener_v3.env()
             env = pad_action_space_v0(env)
             env = pad_observations_v0(env)
             return env
+
         get_env_lambda = lambda config: PettingZooEnv(create_env(config))
         register_env(SPEAKER_LISTENER, lambda config: get_env_lambda(config))
         return get_env_lambda({}), SPEAKER_LISTENER
+
 
 def is_partial_obs_equal_to_state(partial_obs, state):
     if len(partial_obs) != len(state):
