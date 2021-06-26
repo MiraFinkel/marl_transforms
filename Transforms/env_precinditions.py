ccf2e35a24c8cb8fb47d42fc865c993c6738b1ad
nitsan57 <nitsan57@gmail.com>
2021-06-26 17:15:50 +0300

    [PRED CALC] fix action list reference bug

--- Transforms/env_precinditions.py
+++ Transforms/env_precinditions.py
@@ -3,12 +3,8 @@ import numpy as np
 
 
 def calc_action_feature_sets(P, decode, num_actions):
-    # num_actions = env.num_actions
-    # decode = env.decode
-
     # EACH INDEX IN THE LIST IS AN ACTION!
-    action_feature_sets = [set()] * num_actions
-
+    action_feature_sets = [set() for _ in range(num_actions)]  # * num_actions
     for k, v in P.items():
         current_state = k
         current_state_f = decode(current_state)
@@ -16,21 +12,17 @@ def calc_action_feature_sets(P, decode, num_actions):
         y = z.to_numpy()[0]
         arr_2d = np.array([*y])
         new_states_vector = arr_2d[:, 1]
-        for i, s in enumerate(new_states_vector):
+        for action, s in enumerate(new_states_vector):
             state_f = np.array(decode(s))
             feature_diff = current_state_f - state_f
-            action_feature_sets[i].add(tuple(feature_diff))
 
-    # remove all no-diffs(0,0,0,0,...)
-    for i in range(len(action_feature_sets)):
-        first_elem = action_feature_sets[0].pop()
-        action_feature_sets[0].add(first_elem)
-        zero_state = tuple([0] * len(first_elem))
-        try:
-            action_feature_sets[i].remove(zero_state)
-        except:
-            pass
-        return action_feature_sets
+            # only to calc zero feture vec
+            zero_state = tuple([0] * len(state_f))
+            ###
+            if tuple(feature_diff) != zero_state:
+                action_feature_sets[action].add(tuple(current_state_f))
+
+    return action_feature_sets
 
 
 def get_final_pred_dict_from_sets(action_feature_sets, f_names, a_names):
