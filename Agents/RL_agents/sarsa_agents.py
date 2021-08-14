8fe656beaa9a032fe2bbf94c70a8157682d5dc55
MiraFinkel <mirafinkel@gmail.com>
2021-08-14 11:44:40 +0300

    with fixed sarsa agent
    with fixed sarsa agent

--- Agents/RL_agents/sarsa_agents.py
+++ Agents/RL_agents/sarsa_agents.py
@@ -1,3 +1,6 @@
+from rl.agents import SARSAAgent
+from rl.policy import EpsGreedyQPolicy
+
 from Agents.abstract_agent import AbstractAgent
 import numpy as np
 import keras
@@ -8,6 +11,10 @@ import gym
 import random
 from keras.utils import to_categorical
 from tensorflow.keras.optimizers import Adam
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.layers import Dense, Activation, Flatten, Embedding, Reshape
+from constants import *
+
 
 learning_rate = 0.001
 epsilon = 1
@@ -17,7 +24,7 @@ memory = deque(maxlen=1000000)
 min_eps = 0.01
 
 
-class KerasSarsaAgent(AbstractAgent):
+class HandsOnSarsaAgent(AbstractAgent):
     def __init__(self, env, timesteps_per_episode=10001):
         super().__init__(env, timesteps_per_episode)
         self.model = self._build_compile_model()
@@ -66,6 +73,88 @@ class KerasSarsaAgent(AbstractAgent):
         model.add(keras.layers.Dense(64, activation=relu))
         model.add(keras.layers.Dense(1, activation=linear))
         model.compile(loss="mse", optimizer=Adam(lr=learning_rate))
+        return model
+
+    def compute_action(self, state) -> int:
+        """
+        Computes the best action from a given state.
+        Returns: a int that represents the best action.
+        """
+        pass
+
+    def stop_episode(self):
+        pass
+
+    def episode_callback(self, state, action, reward, next_state, terminated):
+        pass
+
+    def evaluate(self):
+        pass
+
+    def replay_experiences(self):
+        if len(memory) >= batch_size:
+            sample_choices = np.array(memory)
+            mini_batch_index = np.random.choice(len(sample_choices), batch_size)
+            # batch = random.sample(memory, batch_size)
+            states, actions, next_states, rewards, finishes = [], [], [], [], []
+            for index in mini_batch_index:
+                states.append(memory[index][0])
+                actions.append(memory[index][1])
+                next_states.append(memory[index][2])
+                rewards.append(memory[index][3])
+                finishes.append(memory[index][4])
+            states = np.array(states)
+            actions = np.array(actions)
+            next_states = np.array(next_states)
+            rewards = np.array(rewards)
+            finishes = np.array(finishes)
+            states = np.squeeze(states)
+            next_states = np.squeeze(next_states)
+            q_vals_next_state = self.model.predict_on_batch(next_states)
+            q_vals_target = self.model.predict_on_batch(states)
+            max_q_values_next_state = np.amax(q_vals_next_state, axis=1)
+            # q_vals_target[np.arange(batch_size), actions] = rewards + gamma * (max_q_values_next_state) * (1 - finishes)
+            tmp = rewards + gamma * max_q_values_next_state * (1 - finishes)
+            tmp = np.reshape(tmp, q_vals_target[actions].shape)
+            q_vals_target[actions] = tmp
+            self.model.fit(states, q_vals_target, verbose=0)
+            global epsilon
+            if epsilon > min_eps:
+                epsilon *= 0.996
+
+
+class KerasSarsaAgent(AbstractAgent):
+    def __init__(self, env, timesteps_per_episode=10001):
+        super().__init__(env, timesteps_per_episode)
+        self.model = self._build_compile_model()
+        self.num_episodes = 400
+        self.evaluating = False
+        self.num_actions = env.num_actions
+        self.num_actions = env.num_states
+        self.model = self._build_compile_model()
+        self.agent = SARSAAgent(model=self.model, nb_actions=self.num_actions, policy=EpsGreedyQPolicy())
+
+    def run(self) -> {str: float}:
+        """
+        The agent's training method.
+        Returns: a dictionary - {"episode_reward_mean": __, "episode_reward_min": __, "episode_reward_max": __,
+        "episode_len_mean": __}
+        """
+        self.agent.compile(Adam(lr=0.001), metrics=["mse"])
+        history = self.agent.fit(self.env, nb_steps=50000, visualize=False, verbose=1)
+        result = {EPISODE_REWARD_MEAN: np.array(history.history["episode_reward"]),
+                  EPISODE_STEP_NUM_MEAN: np.array(history.history["nb_episode_steps"]),
+                  EPISODE_REWARD_MIN: np.empty([]),
+                  EPISODE_REWARD_MAX: np.empty([]), EPISODE_VARIANCE: np.empty([])}
+        return result
+
+    def _build_compile_model(self):
+        model = Sequential()
+        model.add(Flatten(input_shape=(1, self.num_actions)))
+        model.add(Dense(24, activation='relu'))
+        model.add(Dense(16, activation='relu'))
+        model.add(Dense(16, activation='relu'))
+        model.add(Dense(self.num_actions, activation='linear'))
         return model
 
     def compute_action(self, state) -> int:
