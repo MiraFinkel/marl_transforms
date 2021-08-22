081c69d68a054a9b2f928b47ad1c09c5d57b12ee
MiraFinkel <mirafinkel@gmail.com>
2021-08-22 11:15:40 +0300

    with new success rate
    with new success rate

--- Transforms/taxi_transform_search_env.py
+++ Transforms/taxi_transform_search_env.py
@@ -55,8 +55,8 @@ class SingleTaxiTransformAnticipatedSearchEnv(SingleTaxiSimpleEnv):
                         transformed_env = all_single_transformed_envs[transform_name]
                         self.action_translation_dict[new_act] = {ACT: not_allowed_act, IDX: pre_idx, VAL: pre_val}
                         self.P[state][new_act] = transformed_env.P[state][not_allowed_act]
-            align_with_anticipated, anticipated_act = self.is_state_align_with_anticipated_policy(state,
-                                                                                                  anticipated_policy)
+            align_with_anticipated, anticipated_act, _ = is_state_align_with_anticipated_policy(self, state,
+                                                                                                anticipated_policy)
             if align_with_anticipated:
                 next_anticipated_state = self.P[state][anticipated_act][0][1]
                 if state != next_anticipated_state:
@@ -73,32 +73,17 @@ class SingleTaxiTransformAnticipatedSearchEnv(SingleTaxiSimpleEnv):
         self.num_actions = new_act + 1
         discrete.DiscreteEnv.__init__(self, self.num_states, self.num_actions, self.P, self.initial_state_distribution)
 
-    def is_state_align_with_anticipated_policy(self, state, anticipated_policy):
-        for anticipated_state, anticipated_action in anticipated_policy.items():
-            if self.is_state_align_with_anticipated_state(state, anticipated_state):
-                return True, anticipated_action[0]
-        return False, None
-
-    def is_state_align_with_anticipated_state(self, state, anticipated_state):
-        anticipated_state = list(anticipated_state)
-        decoded_state = self.decode(state)
-        for (anticipated_feature, feature) in zip(anticipated_state, decoded_state):
-            if anticipated_feature and feature != anticipated_feature:
-                return False
-        return True
-
-
-if __name__ == '__main__':
-    anticipated_policy = {(2, 0, 0, 3, None): [1],
-                          (1, 0, 0, 3, None): [1],
-                          (0, 0, 0, 3, None): [4]}
-    search_env = SingleTaxiTransformAnticipatedSearchEnv(anticipated_policy)
-    # file_name = "taxi_example_data/taxi_transformed_env/search_env_without_5.pkl"
-    # save_pkl_file(file_name, search_env)
-    #
-    transform_name = "search_anticipated_env"
-    save_pkl_file(transform_name, search_env)
-    # file = open(file_name, "rb")
-    # new_env = pickle.load(file)
-    # generate_agent(SINGLE_TAXI_EXAMPLE, KERAS_DQN, 100, new_env, transform_name)
-    a = 7
+# if __name__ == '__main__':
+#     anticipated_policy = {(2, 0, 0, 3, None): [1],
+#                           (1, 0, 0, 3, None): [1],
+#                           (0, 0, 0, 3, None): [4]}
+#     search_env = SingleTaxiTransformAnticipatedSearchEnv(anticipated_policy)
+#     file_name = "taxi_example_data/taxi_transformed_env/search_env_without_5.pkl"
+#     save_pkl_file(file_name, search_env)
+#
+#     transform_name = "search_anticipated_env"
+#     save_pkl_file(transform_name, search_env)
+#     file = open(file_name, "rb")
+#     new_env = pickle.load(file)
+#     generate_agent(SINGLE_TAXI_EXAMPLE, KERAS_DQN, 100, new_env, transform_name)
+#     a = 7
